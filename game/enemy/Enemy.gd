0861056f09b9bb83e09311bc0fca9c37cc7d0ac5
gijs <gijs.kwaks@hotmail.com>
2017-12-02 17:50:59 +0100

    Enemies rotate

--- game/enemy/Enemy.gd
+++ game/enemy/Enemy.gd
@@ -2,9 +2,11 @@ extends KinematicBody2D
 
 var path = []
 var speed = 200
+var sprite_node
 
 func _ready():
 	set_fixed_process(true)
+	sprite_node = get_node("Sprite")
 
 func _fixed_process(delta):
 	var navigation = get_tree().get_root().get_node("Level/Navigation")
@@ -16,7 +18,9 @@ func _fixed_process(delta):
 		var direction = distance.normalized()
 		
 		var movement_remainder = move(direction * speed * delta)
-	
+		var angle = atan2(direction.x, direction.y)
+		sprite_node.set_rot(angle)
+		
 		if is_colliding():
 			var normal = get_collision_normal()
 			var final_movement = normal.slide(movement_remainder)
