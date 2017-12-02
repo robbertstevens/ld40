5f3cc0229f7504200979affea7709c03a1f76473
Krukas <maikel_martens_1@hotmail.com>
2017-12-02 17:03:45 +0100

    [TASK] Add spawns

--- game/level/LevelOne.gd
+++ game/level/LevelOne.gd
@@ -1,14 +1,21 @@
 extends Node2D
 
 var currentRoom
+var start_time
+var time_elapsed
+
+var max_enemy_count = 3
+var enemy_spawn_rate = 3
 
 func _ready():
 	set_process(true)
+	start_time = OS.get_unix_time()
 	
 	for room in get_tree().get_nodes_in_group("room"):
 		room.connect("body_enter", self, "_enter_roomOne", [room])
 
 func _process(delta):
+	time_elapsed = OS.get_unix_time()
 	if Input.is_action_pressed("key_exit"):
 		get_tree().quit()
 
