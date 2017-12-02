1b859935b1c917b4bf4d203dee0eb34e60a1c4fa
Krukas <maikel_martens_1@hotmail.com>
2017-12-02 14:33:13 +0100

    [TASK] Setup base game

---
+++ game/player/Player.gd
@@ -0,0 +1,73 @@
+extends KinematicBody2D
+
+var sprite_node
+
+var input_x_direction = 0
+var x_direction = 0
+
+var input_y_direction = 0
+var y_direction = 0
+
+const MAX_SPEED = 700
+const ACCELERATION = 2600
+const DECELERATION = 5000
+
+var speed = Vector2()
+var velocity = Vector2()
+
+func _ready():
+	set_process(true)
+	sprite_node = get_node("Sprite")
+	
+func _process(delta):
+	# X movement
+	if input_x_direction:
+		x_direction = input_x_direction
+	
+	if Input.is_action_pressed("move_left"):
+		input_x_direction = -1
+	elif Input.is_action_pressed("move_right"):
+		input_x_direction = 1
+	else:
+		input_x_direction = 0
+	
+	if input_x_direction == - x_direction:
+		speed.x /= 3
+	if input_x_direction:
+		speed.x += ACCELERATION * delta
+	else:
+		speed.x -= DECELERATION * delta
+	speed.x = clamp(speed.x, 0, MAX_SPEED)
+	
+	# Y movement
+	if input_y_direction:
+		y_direction = input_y_direction
+		
+	if Input.is_action_pressed("move_up"):
+		input_y_direction = -1
+	elif Input.is_action_pressed("move_down"):
+		input_y_direction = 1
+	else:
+		input_y_direction = 0
+	
+	if input_y_direction == - y_direction:
+		speed.y /= 3
+	if input_y_direction:
+		speed.y += ACCELERATION * delta
+	else:
+		speed.y -= DECELERATION * delta
+	speed.y = clamp(speed.y, 0, MAX_SPEED)
+
+	velocity = Vector2(speed.x * delta * x_direction, speed.y * delta * y_direction)
+	var movement_remainder = move(velocity)
+	
+	if is_colliding():
+		var normal = get_collision_normal()
+		var final_movement = normal.slide(movement_remainder)
+		speed = normal.slide(speed)
+		move(final_movement)
+	
+	# sprite direction
+	var mouse_pos = get_local_mouse_pos()
+	var angle = atan2(mouse_pos.x, mouse_pos.y)
+	sprite_node.set_rot(angle)
