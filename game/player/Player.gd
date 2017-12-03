c19f8ab5da255f393c170fcc21c27c2623dccac4
Krukas <maikel_martens_1@hotmail.com>
2017-12-03 16:09:05 +0100

    [TASK] Remove old files

--- game/player/Player.gd
+++ game/player/Player.gd
@@ -26,8 +26,6 @@ func _ready():
 func _process(delta):
 	if (health < 0):
 		get_tree().get_root().get_node("Level/Ui").game_over()
-		get_tree().set_pause(true)
-		# get_tree().reload_current_scene()
 	
 	# X movement
 	if input_x_direction:
