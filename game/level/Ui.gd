28e3bb28e43d4f7eacb2415a8c1f636afdf5acf2
Lewis Voncken <lewis@experius.nl>
2017-12-03 19:45:40 +0100

    [FEATURE] Added HighScore Submit!

--- game/level/Ui.gd
+++ game/level/Ui.gd
@@ -17,10 +17,7 @@ func _input(event):
 		get_tree().quit()
 	
 	if event.is_action("start_game") and not game_running:
-		game_running = true
-		game_over_label.set_opacity(0)
-		get_tree().reload_current_scene()
-		get_tree().set_pause(false)
+		start_game()
 	
 	if event.is_action_released("pause_game") and game_running:
 		var pause = not get_tree().is_paused()
@@ -31,8 +28,28 @@ func _input(event):
 		else:
 			pause_label.set_opacity(0)
 
+func start_game():
+	game_running = true
+	game_over_label.set_opacity(0)
+	get_tree().reload_current_scene()
+	get_tree().set_pause(false)
+
 func game_over():
 	game_running = false
 	get_tree().set_pause(true)
 	game_over_label.set_opacity(1)
 	
+func _on_Button_pressed():
+	var name = get_tree().get_root().get_node("Level/Ui/game_over/TextEdit").get_text()
+	var httpClient = HTTPClient.new()
+	httpClient.connect("ld40.robbert.rocks",80)
+	while(httpClient.get_status() == HTTPClient.STATUS_CONNECTING or httpClient.get_status() == HTTPClient.STATUS_RESOLVING):
+		httpClient.poll()
+		print("Connecting...")
+		OS.delay_msec(300)
+	assert(httpClient.get_status() == HTTPClient.STATUS_CONNECTED)
+	var fields = {"name" : name, "score" : get_tree().get_root().get_node("Level/Player").score}
+	var queryString = httpClient.query_string_from_dict(fields)
+	var headers = ["Content-Type: application/x-www-form-urlencoded", "Content-Length: " + str(queryString.length())]
+	var result = httpClient.request(HTTPClient.METHOD_POST,"https://ld40.robbert.rocks/highscore.php", headers, queryString)
+	start_game()
